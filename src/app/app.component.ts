32035b8a2818186db42cc2572fb86e7a09d79397
amnotafraid <amnotafraid@gmail.com>
2017-01-20 19:06:23 -0600

    Lesson 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!, woop woop';
   name = 'ninja';
+  ninja = {
+    name: "Ryu",
+    belt: "Red"
+  };
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   homeTitle = "Welcome to the ninja directory";
-
-  ninja = {
-    name: "Yoshi",
-    belt: "black"
-  };
+  @Input() ninja;
 
   alertMe(val) {
     alert(val);
